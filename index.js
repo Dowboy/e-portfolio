2896afa818cdb562d3e9a1d2119134e8a1ffecb9
Dowboy <alirosewashere@gmail.com>
2022-06-07 01:36:28 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,66 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+// dark-theme
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+// email js for contact modal
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      `service_gj404v3`,
+      "template_k1duk0m",
+      event.target,
+      "zDjGqrs8sa9brdG7S"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert("The email service is temporarily unavailable.");
+    });
+}
+
+// modal visbility toggle
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+  document.documentElement.scrollTop = 0;
+}
+
+// background interaction
+
+const scaleFactor = 1 / 20;
+
+function moveBackground() {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
